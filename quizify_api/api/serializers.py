4f06c075a5b0ccb7258aa016bfd45fe6dcd34f71
Morten Noddeland <morten@noddeland.no>
2016-03-11 19:29:23 +0100

    Modified the serializer

--- quizify_api/api/serializers.py
+++ quizify_api/api/serializers.py
@@ -2,14 +2,17 @@ from rest_framework import serializers
 
 from api.models import Category, Song
 
-class CategorySerializer(serializers.HyperlinkedModelSerializer):
-    songs = serializers.StringRelatedField(many=True)
-
-    class Meta:
-        model = Category
-        fields = ('id', 'name', 'songs')
 
 class SongSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Song
         fields = ('id', 'spotify_uri')
+
+
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    # songs = serializers.StringRelatedField(many=True)
+    songs = SongSerializer(many=True)
+
+    class Meta:
+        model = Category
+        fields = ('id', 'name', 'songs')
