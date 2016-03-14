f7d6107446b90139f324882792ab341b43167940
Morten Noddeland <morten@noddeland.no>
2016-03-14 17:21:04 +0100

    Addded Round, Score and Question to the admin panel

--- quizify_api/api/admin.py
+++ quizify_api/api/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from api.models import Category, Song, Game
+from api.models import *
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
-
 
 class SongAdmin(admin.ModelAdmin):
     list_display = ('spotify_uri',)
@@ -11,6 +10,18 @@ class SongAdmin(admin.ModelAdmin):
 class GameAdmin(admin.ModelAdmin):
     list_display = ('player1','player2','invitation_status',)
 
+class RoundAdmin(admin.ModelAdmin):
+    list_display = ('category', 'status', 'whos_turn', 'whos_turn', 'winner',)
+
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ('player', 'answer_time', 'answered_correctly',)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('round', 'correct_answer', 'score_player1', 'score_player2',)
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Song, SongAdmin)
 admin.site.register(Game, GameAdmin)
+admin.site.register(Round, RoundAdmin)
+admin.site.register(Score, ScoreAdmin)
+admin.site.register(Question, QuestionAdmin)
