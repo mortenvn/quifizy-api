a5dc92170d911f60641e2c513184cbf6069bcfce
Morten Noddeland <morten@noddeland.no>
2016-04-15 15:18:12 +0200

    Unfinished implementation of the registration process

--- quizify_api/accounts/views.py
+++ quizify_api/accounts/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.conf import settings
+from django.contrib.auth.models import User
 
-# Create your views here.
+from serializers import RegisterUserSerializer, PlayerSerializer
+from models import Player
+from oauth2_provider.models import Application, AccessToken
+
+@api_view(['POST'])
+def register(request):
+    qp = RegisterUserSerializer(data=request.data)
+    if not qp.is_valid():
+        return Response(data=qp.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    username = qp['username'].value
+    email = qp['email'].value
+    password = qp['password'].value
+
+    user = User.objects.create_user(username, email, password)
+    player = Player.objects.create(user=user)
+
+    auth_app_name = settings.AUTH_APPLICATION_NAME
+    app = Application.objects.get(name=auth_app_name)
+    token, created = AccessToken.objects.get_or_create(user=user)
+
+    return Response({'token': token.key}, status=status.HTTP_201_CREATED)
+
+
+
+    # serialized = UserSerializer(data=request.DATA)
+    # if serialized.is_valid():
+    #     User.objects.create_user(
+    #         serialized.init_data['email'],
+    #         serialized.init_data['username'],
+    #         serialized.init_data['password']
+    #     )
+    #     return Response(serialized.data, status=status.HTTP_201_CREATED)
+    # else:
+    #     return Response(serialized._errors, status=status.HTTP_400_BAD_REQUEST)
