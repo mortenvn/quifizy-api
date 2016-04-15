c943fb88d08da56ef9fba06b48e772cc1c709c44
andrroy <andreas@royrvik.org>
2016-04-15 20:12:23 +0200

    Player object is now returned when registering. Should be replaced by AccessToken later on

--- quizify_api/accounts/views.py
+++ quizify_api/accounts/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.utils.timezone import now, timedelta
 
 from serializers import RegisterUserSerializer, PlayerSerializer
 from models import Player
@@ -21,11 +22,16 @@ def register(request):
     user = User.objects.create_user(username, email, password)
     player = Player.objects.create(user=user)
 
-    auth_app_name = settings.AUTH_APPLICATION_NAME
-    app = Application.objects.get(name=auth_app_name)
-    token, created = AccessToken.objects.get_or_create(user=user)
+    # TODO: Return auth token
+    # auth_app_name = settings.AUTH_APPLICATION_NAME
+    # app = Application.objects.get(name=auth_app_name)
+    # token, created = AccessToken.objects.get_or_create(user=user, expires=now() + timedelta(days=1), application=app)
+    # print "========"
+    # print token
+    # print created
+    # print "========"
 
-    return Response({'token': token.key}, status=status.HTTP_201_CREATED)
+    return Response({'player': PlayerSerializer(player).data}, status=status.HTTP_201_CREATED)
 
 
 
