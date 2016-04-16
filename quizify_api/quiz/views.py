12c9e9d3ab2ad56f60539961423b72374ade21a3
andrroy <andreas@royrvik.org>
2016-04-16 14:23:50 +0200

    Categories and Games can only be seen if authenticated. Only return Games user is part of

--- quizify_api/quiz/views.py
+++ quizify_api/quiz/views.py
@@ -1,14 +1,28 @@
-from django.shortcuts import render
 from rest_framework import viewsets
+
+from permissions import PartOfGame
 from serializers import CategorySerializer, SongSerializer, GameSerializer
-from songs.models import Category, Song
-from quiz.models import *
+from models import Game
+from songs.models import Category
+from rest_framework import permissions
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from django.db.models import Q
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
+    http_method_names = ('get',)
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (permissions.IsAuthenticated,)
     serializer_class = CategorySerializer
 
 
 class GameViewSet(viewsets.ModelViewSet):
     queryset = Game.objects.all()
+    http_method_names = ('get', 'put', 'post',)
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (permissions.IsAuthenticated, PartOfGame,)
     serializer_class = GameSerializer
+
+    def get_queryset(self):
+        player = self.request.user.player
+        return Game.objects.filter(Q(player1=player) | Q(player2=player))
