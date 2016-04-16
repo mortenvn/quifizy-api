babefce990dcd612ea70a802443b6c3cc0e12f43
andrroy <andreas@royrvik.org>
2016-04-16 14:24:51 +0200

    Cleanup imports

--- quizify_api/quiz/views.py
+++ quizify_api/quiz/views.py
@@ -1,12 +1,13 @@
 from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from django.db.models import Q
 
 from permissions import PartOfGame
 from serializers import CategorySerializer, SongSerializer, GameSerializer
 from models import Game
 from songs.models import Category
-from rest_framework import permissions
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from django.db.models import Q
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
