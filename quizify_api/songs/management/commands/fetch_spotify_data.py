4d12060b4bc12169b07a0e601ff34bc626a0991d
andrroy <andreas@royrvik.org>
2016-04-18 00:35:55 +0200

    Add custom command to fetch missing song data from spotify

---
+++ quizify_api/songs/management/commands/fetch_spotify_data.py
@@ -0,0 +1,23 @@
+import requests
+from django.core.management import BaseCommand
+from songs.models import Song
+
+
+class Command(BaseCommand):
+    help = "Andreas tester"
+
+    def get_spotify_data(self, spotify_uri):
+        r = requests.get('https://api.spotify.com/v1/tracks/' + spotify_uri)
+        return r.json()
+
+    def handle(self, *args, **options):
+        songs = Song.objects.all()
+        for song in songs:
+            if not song.url or not song.artist or not song.name:
+                data = self.get_spotify_data(song.spotify_uri)
+                print data
+                song.artist = data['artists'][0]['name']
+                song.name = data['name']
+                song.url = data['preview_url']
+                song.save()
+        self.stdout.write("Done")
