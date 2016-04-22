73121ead2e999d5de7c748056a453ebe72468c6f
andrroy <andreas@royrvik.org>
2016-04-22 14:54:28 +0200

    Check if game between player 1 and 2 already exists

--- quizify_api/quiz/views.py
+++ quizify_api/quiz/views.py
@@ -92,6 +92,11 @@ class GameViewSet(viewsets.ModelViewSet):
         if player2.id == request.user.player.id:
             return Response(data={"non_field_errors": ["Player cannot play itself"]}, status=status.HTTP_400_BAD_REQUEST)
 
+        # Check if a game between the two players already exists
+        existing_games = Game.objects.filter(Q(player1=request.user.player, player2=player2) | Q(player2=request.user.player, player1=player2))
+        if existing_games is not None:
+            return Response(data={"non_field_errors": ["Game between player1 and player2 already exists"]}, status=status.HTTP_400_BAD_REQUEST)
+
         # TODO: Send notification to player2
         new_game = Game.objects.create(player1=request.user.player, player2=player2, invitation_status='sent')
         game_score = OverallScore.objects.create(game=new_game)
